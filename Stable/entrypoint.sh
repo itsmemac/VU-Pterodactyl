fc6a7c31dd1500b9e47e36bb2ac3b1e5e583b1e9
Mac <37410297+itsmemac@users.noreply.github.com>
2025-02-15 01:52:08 +0530

    possible xvfb error fix

--- Stable/entrypoint.sh
+++ Stable/entrypoint.sh
@@ -12,47 +12,52 @@ export INTERNAL_IP
 
 # Start Xvfb if enabled
 if [[ $XVFB == 1 ]]; then
-    Xvfb :0 -screen 0 ${DISPLAY_WIDTH}x${DISPLAY_HEIGHT}x${DISPLAY_DEPTH} &
+    Xvfb :0 -screen 0 1024x768x16 &
+    export DISPLAY=:0
 fi
 
-# Create Wine prefix directory if it doesn't exist
-echo "First launch will throw some errors. Ignore them"
-
+# Ensure correct permissions for Wine prefix
+echo "Setting correct permissions for Wine prefix"
 mkdir -p $WINEPREFIX
+chown -R $(whoami) $WINEPREFIX
+export WINEDEBUG=-all
+
+# Initialize Wine prefix
+wineboot --init
 
 # Check if wine-gecko required and install it if so
 if [[ $WINETRICKS_RUN =~ gecko ]]; then
-        echo "Installing Gecko"
-        WINETRICKS_RUN=${WINETRICKS_RUN/gecko}
+    echo "Installing Gecko"
+    WINETRICKS_RUN=${WINETRICKS_RUN/gecko}
 
-        if [ ! -f "$WINEPREFIX/gecko_x86.msi" ]; then
-                wget -q -O $WINEPREFIX/gecko_x86.msi http://dl.winehq.org/wine/wine-gecko/2.47.4/wine_gecko-2.47.4-x86.msi
-        fi
+    if [ ! -f "$WINEPREFIX/gecko_x86.msi" ]; then
+        wget -q -O $WINEPREFIX/gecko_x86.msi http://dl.winehq.org/wine/wine-gecko/2.47.4/wine_gecko-2.47.4-x86.msi
+    fi
 
-        if [ ! -f "$WINEPREFIX/gecko_x86_64.msi" ]; then
-                wget -q -O $WINEPREFIX/gecko_x86_64.msi http://dl.winehq.org/wine/wine-gecko/2.47.4/wine_gecko-2.47.4-x86_64.msi
-        fi
+    if [ ! -f "$WINEPREFIX/gecko_x86_64.msi" ]; then
+        wget -q -O $WINEPREFIX/gecko_x86_64.msi http://dl.winehq.org/wine/wine-gecko/2.47.4/wine_gecko-2.47.4-x86_64.msi
+    fi
 
-        wine msiexec /i $WINEPREFIX/gecko_x86.msi /qn /quiet /norestart /log $WINEPREFIX/gecko_x86_install.log
-        wine msiexec /i $WINEPREFIX/gecko_x86_64.msi /qn /quiet /norestart /log $WINEPREFIX/gecko_x86_64_install.log
+    wine msiexec /i $WINEPREFIX/gecko_x86.msi /qn /quiet /norestart /log $WINEPREFIX/gecko_x86_install.log
+    wine msiexec /i $WINEPREFIX/gecko_x86_64.msi /qn /quiet /norestart /log $WINEPREFIX/gecko_x86_64_install.log
 fi
 
 # Check if wine-mono required and install it if so
 if [[ $WINETRICKS_RUN =~ mono ]]; then
-        echo "Installing mono"
-        WINETRICKS_RUN=${WINETRICKS_RUN/mono}
+    echo "Installing mono"
+    WINETRICKS_RUN=${WINETRICKS_RUN/mono}
 
-        if [ ! -f "$WINEPREFIX/mono.msi" ]; then
-                wget -q -O $WINEPREFIX/mono.msi https://dl.winehq.org/wine/wine-mono/9.1.0/wine-mono-9.1.0-x86.msi
-        fi
+    if [ ! -f "$WINEPREFIX/mono.msi" ]; then
+        wget -q -O $WINEPREFIX/mono.msi https://dl.winehq.org/wine/wine-mono/9.1.0/wine-mono-9.1.0-x86.msi
+    fi
 
-        wine msiexec /i $WINEPREFIX/mono.msi /qn /quiet /norestart /log $WINEPREFIX/mono_install.log
+    wine msiexec /i $WINEPREFIX/mono.msi /qn /quiet /norestart /log $WINEPREFIX/mono_install.log
 fi
 
 # List and install other packages
 for trick in $WINETRICKS_RUN; do
-        echo "Installing $trick"
-        winetricks -q $trick
+    echo "Installing $trick"
+    winetricks -q $trick
 done
 
 # Replace Startup Variables
